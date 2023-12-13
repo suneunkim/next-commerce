28f7bd2a0fa766a53f49aeee1e902fd22f19131d
suneunkim <tjsdms89@gmail.com>
2023-12-13 17:20:09 +0900

    쿠폰 일부 수정

--- src/components/Coupon.tsx
+++ src/components/Coupon.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
-import { useCalculator } from "@/hooks/\buseCalculator";
 
 const Coupon = () => {
+  const [hidden, setHidden] = useState(false);
+
   const handleClick = () => {
     alert("쿠폰을 얻었습니다!");
+    setHidden(true);
   };
 
   const controls = useAnimation();
-  const myElementRef = useRef(null);
-
-  const getRandomNumber = (min: number, max: number) =>
-    Math.random() * (max - min) + min;
 
   const animateCoupon = async () => {
     // 1시간에 5초 동안 나타나고 사라지는 효과
@@ -19,38 +17,36 @@ const Coupon = () => {
 
     // 불규칙한 위치로 이동하는 애니메이션 추가
     await controls.start({
-      x: [0, 1000, -1000, 0],
-      y: [0, 1000, -1000, 0],
-      transition: { duration: 30, ease: "linear" },
+      x: [0, 500, -500, -600, 400, 0],
+      y: [0, 800, 1300, -500, 200, 0],
+      transition: { duration: 20, ease: "linear" },
     });
     await controls.start({ opacity: 0 });
-
-    // 초기 위치로
-    await controls.start({
-      x: 0,
-      y: 0,
-      opacity: 0,
-      transition: { duration: 0 },
-    });
+    setHidden(true);
   };
 
   useEffect(() => {
-    // const intervalId = setInterval(animateCoupon, 3000);
-    // return () => clearInterval(intervalId);
     animateCoupon();
+    // const intervalId = setInterval(animateCoupon, 300000);
+    // return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div className="w-screen h-screen flex justify-center absolute z-30">
-      <motion.div
-        className="w-32 h-32 rounded-full bg-yellow-400 border-none flex flex-col justify-center items-center text-white text-xl font-bold cursor-pointer"
-        id="coupon"
-        onClick={handleClick}
-        animate={controls}
-      >
-        <div>깜짝 쿠폰!</div>
-      </motion.div>
-    </div>
+    <>
+      {!hidden && (
+        <div className="w-full flex justify-center absolute z-30">
+          <motion.div
+            className="w-32 h-32 rounded-full bg-yellow-400 border-none flex flex-col justify-center items-center text-white text-xl font-bold cursor-pointer"
+            id="coupon"
+            onClick={handleClick}
+            initial={{ opacity: 0 }}
+            animate={controls}
+          >
+            <div>깜짝 쿠폰!</div>
+          </motion.div>
+        </div>
+      )}
+    </>
   );
 };
 
