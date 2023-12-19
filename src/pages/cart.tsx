3949401140bba77efb8f4d50c9825008b97e0c97
suneunkim <tjsdms89@gmail.com>
2023-12-19 19:19:48 +0900

    feat: 장바구니 기능 다듬기

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -31,6 +31,34 @@ const cart = () => {
     }
   }, [selectedList]);
 
+  // 선택한 옵션 장바구니에서 삭제하기
+  const handleRemoveOption = (seletedOption: string) => {
+    setSelectedList((prev) => {
+      const updatedList = prev.filter((item) => item.value !== seletedOption);
+
+      const existiongCartData = localStorage.getItem("cartData");
+      const existingData = existiongCartData
+        ? JSON.parse(existiongCartData)
+        : {};
+
+      const cartData = {
+        ...existingData,
+        storedList: updatedList,
+      };
+      localStorage.setItem("cartData", JSON.stringify(cartData));
+      return updatedList;
+    });
+  };
+
+  const calculateTotalAmount = () => {
+    return selectedList.reduce((acc, selectedOption) => {
+      const totalAmout = selectedOption.quantity * selectedOption.price;
+      return acc + totalAmout;
+    }, 0);
+  };
+
+  const deliveryTotalAmout = calculateTotalAmount() < 50000;
+
   if (cartData.storedList.length === 0) {
     return (
       <div className="w-[1300px] pt-[113px] mx-auto h-full text-sm px-10">
@@ -43,7 +71,7 @@ const cart = () => {
   }
   return (
     <div className="w-[1300px] pt-[113px] mx-auto h-full text-sm px-10">
-      <div className="text-4xl text-center p-28">CART</div>
+      <div className="text-4xl text-center p-20">CART</div>
       <div className="bg-gray-200/50 p-3 border font-semibold">{`일반 상품(${cartData.storedList.length})`}</div>
       <div
         className="grid grid-cols-[0.2fr,0.7fr,1fr] gap-5 p-3 text-center border-b
@@ -91,10 +119,45 @@ const cart = () => {
             <span className="font-semibold">
               {(item.price * item.quantity).toLocaleString("ko-KR")}원
             </span>
-            <span>선택</span>
+            <span className="text-xs space-y-1">
+              <div className="bg-blue-800 text-white p-1">주문하기</div>
+              <div className="bg-gray-200 p-1">위시리스트</div>
+              <div
+                onClick={() => handleRemoveOption(item.value)}
+                className="border border-gray-400 p-1 cursor-pointer"
+              >
+                삭제하기
+              </div>
+            </span>
           </div>
         </div>
       ))}
+      <div className="mt-20">
+        <div className="font-semibold grid grid-cols-[0.2fr,0.2fr,0.6fr] gap-5 p-5 text-center border-t">
+          <span>총 상품금액</span>
+          <span>총 배송비</span>
+          <span>결제예정금액</span>
+        </div>
+        <div className="text-xl font-semibold grid grid-cols-[0.2fr,0.2fr,0.6fr] gap-5 p-5 text-center border-t">
+          <span> {calculateTotalAmount().toLocaleString("ko-KR")}원</span>
+          <div className="flex flex-col relative">
+            <span>+ {deliveryTotalAmout ? "3,000" : "0"}</span>
+            {deliveryTotalAmout && (
+              <span className="text-sm  bg-yellow-400 rounded-full p-2 mt-3">
+                5만원 이상 구매하면 무료배송!
+              </span>
+            )}
+          </div>
+          <span className="text-blue-800">
+            ={" "}
+            {(deliveryTotalAmout
+              ? 3000 + calculateTotalAmount()
+              : calculateTotalAmount()
+            ).toLocaleString("ko-KR")}
+            원
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
